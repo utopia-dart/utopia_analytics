fa8fa54571ddf40f2307216b710be52254244184
Damodar Lohani <dlohani48@gmail.com>
2023-02-18 00:50:55 +0000

    create package

---
+++ example/utopia_analytics_example.dart
@@ -0,0 +1,6 @@
+import 'package:utopia_analytics/utopia_analytics.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/utopia_analytics.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library utopia_analytics;
+
+export 'src/utopia_analytics_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
